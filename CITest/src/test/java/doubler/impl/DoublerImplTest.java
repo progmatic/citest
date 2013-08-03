e05f776db533c944621b574f0ff4a9b639502e15
gooferator <gooferator@gmail.com>
2013-08-03 12:43:45 +0100

    a

--- CITest/src/test/java/doubler/impl/DoublerImplTest.java
+++ CITest/src/test/java/doubler/impl/DoublerImplTest.java
@@ -8,6 +8,6 @@ public class DoublerImplTest {
     @Test
     public void testIt() {
         Doubler doubler = new DoublerImpl();
-        assert doubler.doubleIt(2) == 4;
+        assert doubler.doubleIt(2) == 5;
     }
 }
